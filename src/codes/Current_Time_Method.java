58d54dc39160dacc9c62c82d40e138c24de9a5b4
mginham <45317731+mginham@users.noreply.github.com>
2018-12-31 20:20:31 -0500

    Committing Current Time Assignment from Computer Science 30

---
+++ src/codes/Current_Time_Method.java
@@ -0,0 +1,13 @@
+package codes;
+
+public class Current_Time_Method {
+
+	public static void main(String[] args) {
+		// System.currentTimeMillis()
+		// This method returns the difference, measured in milliseconds,
+		// between the current time and midnight, January 1, 1970 UTC(coordinated universal time)
+		
+		System.out.println("Current Time in milliseconds = " + System.currentTimeMillis());
+	} // end main
+
+}
